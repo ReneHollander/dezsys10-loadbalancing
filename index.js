57766393accb4c3536fe1430195c0780b1ce7763
Rene Hollander <rene.hollander@hotmail.de>
2016-02-12 12:55:55 +0100

    finishes round robin implementation

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 
 const minimist = require('minimist');
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const LoadBalancer = require('./lib/loadbalancer');
 const RoundRobin = require('./lib/strategies/roundrobin');
 const Service = require('./lib/service');
 
 let loadBalancer = new LoadBalancer(5001, new RoundRobin());
-let service = new Service('http://localhost:5001', 'service1');
+new Service('http://localhost:5001', 'service1');
+new Service('http://localhost:5001', 'service2');
 
 let koa = new Koa();
+let router = new Router();
 
-koa.use(function *() {
-    loadBalancer.execute({title: "Hello World"});
-    this.body = "Hello World!";
+router.get('/', function *(next) {
+    this.body = yield loadBalancer.execute({hello: "world"});
+    yield next;
 });
+
+koa.use(router.routes());
+koa.use(router.allowedMethods());
 
 koa.listen(5000);
