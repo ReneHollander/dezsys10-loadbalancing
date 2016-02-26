539f6b260c076bda3410d17b895594471e10b07a
Rene Hollander <rene.hollander@hotmail.de>
2016-02-26 11:10:01 +0100

    use xz instead of lz gmp archive

--- build-deps.sh
+++ build-deps.sh
@@ -4,9 +4,9 @@ if [ ! -d "deps/" ]; then
   mkdir -p deps/
   cd deps/
 
-  wget https://gmplib.org/download/gmp/gmp-6.1.0.tar.lz
-  tar xfv gmp-6.1.0.tar.lz
-  rm gmp-6.1.0.tar.lz
+  wget https://gmplib.org/download/gmp/gmp-6.1.0.tar.xz
+  tar xfv gmp-6.1.0.tar.xz
+  rm gmp-6.1.0.tar.xz
   mv gmp-6.1.0/ gmp/
   cd gmp/
   ./configure CFLAGS='-fPIC'
